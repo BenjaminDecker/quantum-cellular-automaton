723b2545fd1297a44668203c4f393a33ec24fea3
Benjamin Decker <benjamin.decker@tum.de>
2022-07-08 19:09:07 +0200

    Reduce colorbar spacing in plots even more

--- plot.py
+++ plot.py
@@ -32,7 +32,16 @@ def plot(heatmaps, path):
         for index, heatmap in enumerate(heatmaps):
             pcm = axs[index].pcolormesh(
                 heatmap.T, cmap=colormap, vmin=0., vmax=1.)
-            axs[index].set_aspect("equal")
+            if args.NUM_STEPS < 50:
+                axs[index].set_aspect("equal")
 
         fig.colorbar(pcm, ax=axs[:])
+
+        # I use subplots_adjust to move the colorbar closer to the heatmaps
+        # I don't know why, but .77 and .78 seem to give good results for their respective num_cells-ranges
+        if args.NUM_STEPS <= 200:
+            plt.subplots_adjust(right=.77)
+        else:
+            plt.subplots_adjust(right=.78)
+
         plt.savefig(path, bbox_inches="tight")
