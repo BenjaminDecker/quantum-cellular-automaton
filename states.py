Fix gradient state

states.py
@@ -1,6 +1,7 @@
 from random import random
 from parameters import Parser
 from MPS import MPS
+import numpy as np
 
 args = Parser.instance()
 
@@ -54,7 +55,10 @@ def equal_superposition_but_outer():
 
 
 def gradient(reverse=False):
-    plist = [i / (args.rules.ncells - 1) for i in range(args.rules.ncells)]
+    plist = [
+        np.sin(np.pi * i / (args.rules.ncells - 1) / 2) for i in range(args.rules.ncells)
+    ]
+    print(plist)
     return MPS.from_density_distribution(plist=list(reversed(plist)) if reverse else plist)
 
 