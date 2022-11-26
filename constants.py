dac3b91731cf97a841ddfe485ea5a1c63d35a497
Benjamin Decker <benjamin.decker@tum.de>
2022-11-27 00:26:04 +0100

    Add some gates to constants.py

--- constants.py
+++ constants.py
@@ -21,6 +21,11 @@ PROJECTION_KET_0 = np.array([[1., 0.], [0., 0.]])
 PROJECTION_KET_1 = np.array([[0., 0.], [0., 1.]])
 S_OPERATOR = LOWERING_OPERATOR + RISING_OPERATOR
 
+X_GATE = np.array([[0., 1.], [1., 0.]])
+Y_GATE = np.array([[0., -1j], [1j, 0.]])
+Z_GATE = np.array([[1., 0.], [0., -1.]])
+HADAMARD_GATE = 2**(-0.5) * np.array([[1., 1.], [1., -1.]])
+
 
 def Rx_gate(theta):
     return np.array([
